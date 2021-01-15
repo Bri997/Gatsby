f349a162b8422b21785155e678b2d83a0089d1cd
Bri997 <brian.musial@gmail.com>
2021-01-14 21:29:06 -0600

    removed secondary src folder

---
+++ src/pages/404.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function FourOhFourPage() {
+  return (
+    <div>
+      <h1>Hey that's not around...</h1>
+    </div>
+    
+  );
+}
--- src/src/pages/slicemasters.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function SlicemastersPage() {
-  return (
-    <div>
-      <h1>Hey Slicemasters Page...</h1>
-    </div>
-  );
-}
