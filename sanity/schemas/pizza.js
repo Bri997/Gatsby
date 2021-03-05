09ceb347f8c31d37f7427a7737df0e0b07fd8ba5
Bri997 <brian.musial@gmail.com>
2021-03-05 15:53:55 -0600

    sanity custom components

--- sanity/schemas/pizza.js
+++ sanity/schemas/pizza.js
@@ -1,5 +1,6 @@
 import { rules } from "eslint-config-prettier"
-import {MdLocalPizza as icon} from "react-icons/md"
+import { MdLocalPizza as icon } from "react-icons/md"
+import PriceInput from "../components/PriceInputs"
 
 export default {
     name: "pizza",
@@ -10,6 +11,7 @@ export default {
         name: "name",
         title: "Pizza Name",
         type: "string",
+       
         description: "Name of the pizza"
     }, {
             name: "slug",
@@ -33,7 +35,8 @@ export default {
         title: "Price",
         type: "number",
             description: "Price of the pizza in cents",
-        validation: Rule => Rule.min(10000).max(50000)
+            validation: Rule => Rule.min(1000).max(50000),
+         inputComponent: PriceInput,
         },
         {
             name: 'toppings',
@@ -46,13 +49,16 @@ export default {
         select: {
             title: 'name',
             media: 'image',
+            // add veggie to display : leaf if fully veggie
             topping0: "toppings.0.name",
             topping1: "toppings.1.name",
             topping2: "toppings.2.name",
             topping3: "toppings.3.name",
         },
         prepare: ({ title, media, ...toppings }) => {
+
             const filteredToppings = Object.values(toppings).filter(Boolean)
+          
             return {
                 title,
                 media,
