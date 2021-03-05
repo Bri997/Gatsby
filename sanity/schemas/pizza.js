043edc91f3e1ad0cdfe30f1e91cb9bbf28c3023a
Bri997 <brian.musial@gmail.com>
2021-03-04 21:10:50 -0600

    updated sanity schema to create reference to data

--- sanity/schemas/pizza.js
+++ sanity/schemas/pizza.js
@@ -35,6 +35,31 @@ export default {
             description: "Price of the pizza in cents",
         validation: Rule => Rule.min(10000).max(50000)
         },
-    ]
+        {
+            name: 'toppings',
+            title: "Toppings",
+            type: "array",
+            of:[{type: 'reference', to: [{type: 'topping'}]}]
+        }
+    ],
+    preview: {
+        select: {
+            title: 'name',
+            media: 'image',
+            topping0: "toppings.0.name",
+            topping1: "toppings.1.name",
+            topping2: "toppings.2.name",
+            topping3: "toppings.3.name",
+        },
+        prepare: ({ title, media, ...toppings }) => {
+            const filteredToppings = Object.values(toppings).filter(Boolean)
+            return {
+                title,
+                media,
+                subtitle: filteredToppings.join(", ")
+            }
+
+        }
+    }
 
 }
